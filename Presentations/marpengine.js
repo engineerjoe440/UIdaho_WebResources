b282cdf3b294f8ec32392069905e98242c82f3f7
Joe Stanley <engineerjoe440@gmail.com>
2021-02-16 20:36:51 -0800

    added baseline theme and marp engine

---
+++ Presentations/marpengine.js
@@ -0,0 +1,19 @@
+const { Marp } = require('@marp-team/marp-core')
+const hljs = require('highlight.js')
+const iecst = require('highlightjs-structured-text')
+hljs.registerLanguage("iecst", iecst)
+
+module.exports = (opts) => {
+  const marp = new Marp(opts)
+
+  marp.highlighter = (code, lang) => {
+    if (lang) {
+      return hljs.getLanguage(lang)
+        ? hljs.highlight(lang, code, true).value
+        : ''
+    }
+    return hljs.highlightAuto(code).value
+  }
+
+  return marp
+}
